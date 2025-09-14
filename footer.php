4070ef9c95d5ea3c8536b8b38c377b241f48ac5e
AmriShanab <amrishanab@gmail.com>
2025-09-14 12:28:20 +0530

    Latest Fixes

--- footer.php
+++ footer.php
@@ -91,12 +91,6 @@
     <!-- TrustBox script -->
     <script type="text/javascript" src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" async></script>
 
-
-
-
-
-
-
     <!-- Bootstrap JS and Popper.js -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script> -->
