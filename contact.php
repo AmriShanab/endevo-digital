18e5bdecb691ec9086110529e54a6d5eee63afb9
AmriShanab <amrishanab@gmail.com>
2025-09-19 16:53:09 +0530

    Email Settings

--- contact.php
+++ contact.php
@@ -22,8 +22,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $mail->SMTPAuth   = true;
     $mail->Username   = 'hello@endevodigital.com';
     $mail->Password   = 'Endevo@5'; // Replace with your real password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-    $mail->Port = 465;
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
 
 
     // Recipients
